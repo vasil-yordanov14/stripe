96ade9fed7745ef50f426cd8fe675cb6b9a63dd1
vasil-yordanov14 <vasil_sh@abv.bg>
2022-07-24 22:46:03 +0300

    submenu links

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  const { openSidebar, openSubmenu, closeSidebar } = useGlobalContext();
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
   const displaySubmenu = (e) => {
     const page = e.target.textContent;
     const tempBtn = e.target.getBoundingClientRect();
@@ -12,8 +12,13 @@ const Navbar = () => {
     const bottom = tempBtn.bottom - 3;
     openSubmenu(page, { center, bottom });
   };
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains(`link-btn`)) {
+      closeSubmenu();
+    }
+  };
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="stripe" />
