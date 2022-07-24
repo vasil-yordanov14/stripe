0585900b88d967a50e65b373b964bff9247b939b
vasil-yordanov14 <vasil_sh@abv.bg>
2022-07-24 21:37:25 +0300

    hero

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,26 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+            itaque accusamus nisi maiores minus! Fugit repudiandae ipsam quod
+            obcaecati dolore minima eos, aliquam impedit? Similique.
+          </p>
+          <button className="btn">start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="home" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
